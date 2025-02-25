d5bedba2a5adfff6e0bbed2775b0a4b48ed29b5d
mmaria.sbn@gmail.com <maria.sabrina@a.ucb.br>
2025-02-25 11:05:55 -0300

    aula1

---
+++ variaveis_tipo/variaveis_tipos.js
@@ -0,0 +1,37 @@
+//variáveis 
+
+const n = 3; // não muda, sempre constante
+//n=5;
+//var n = 2;
+
+var j = 2; // pode declarar a mesma variavél com outro valor
+var j = 3;
+
+let i = 4; // o valor da varíael pode ser redefinido 
+i = 6;
+
+console.log ('j=' +j);
+
+let nome = "Maria";
+cpf = '123';
+telefone = '123';
+
+let tString= "Meu nome é "+nome;
+
+console.log(tString);
+
+let tpString= `Meu cpf é ${cpf}`; //shift + ´´
+console.log(tpString);
+
+let verdadeiro = true;
+let falso = false;
+
+let conexao = null; //valor que a conexao deu errada
+let conexao2;
+
+let x = 5.0;
+x = .5;
+x = 3;
+
+let  y = x-x;
+console.log('y='+y);
