4ce09093e47525a51682e7346d8a95c4c424e336
@mariassbrina <mmaria.sbn@gmail.com>
2025-03-04 23:03:40 -0300

    atividade do dia 25

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,24 @@
+//soma
+var a = 10, b = 21;
+var soma = a+b;
+console.log ( `O resultado da soma entre ${a} e ${b} é de ${soma}. ` );
+
+//subtracao
+var a = 11, b = 2;
+var subtracao = a-b;
+console.log ( `O resultado da subtracao entre ${a} e ${b} é de ${subtracao}. ` );
+
+//multiplicacao
+var a = 30, b = 2;
+var multiplicacao = a*b;
+console.log ( `O resultado da multiplicacao entre ${a} e ${b} é de ${multiplicacao}. ` );
+
+//divisao
+var a = 45, b = 5;
+var divisao = a/b;
+console.log ( `O resultado da divisao entre ${a} e ${b} é de ${divisao}. ` );
+
+//modulo: resto da divisão
+var a = 17, b = 9;
+var modulo = a%b;
+console.log ( `O resultado do modulo entre ${a} e ${b} é de ${modulo}. ` );
